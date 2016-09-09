fc146bd01191498d4b77ca89077c9e7ffa269be1
MFernflower <machacker9000@gmail.com>
2016-09-08 20:44:00 -0400

    Update dormouse.py

--- dormouse.py
+++ dormouse.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python 
 
 # Dormouse v1 by Mandrake Fernflower, 2016
-# Free to use and modify - just leave this message intact
+# Free to use and modify - just leave this notice intact
 # Made for a RPI3 with a SenseHat
+
 import os
 import time
 from sense_hat import SenseHat
@@ -13,17 +14,17 @@ sensebrd.rotation = 180
 
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
-    return(res.replace("temp=","").replace("'C\n",""))
+    return(res.replace("temp=","").replace("'C\n","C"))
 
 while True:
    sensebrd.clear()
-   sensebrd.show_message("CPU " + getCPUtemperature() + "C", text_colour=[0, 148, 200])
+   sensebrd.show_message("CPU " + getCPUtemperature(), text_colour=[0, 148, 200])
    time.sleep(8)
-   etempraw = sensebrd.get_temperature_from_humidity()
-   etemp = round(etempraw, 1)
+   #etempraw = sensebrd.get_temperature_from_humidity()
+   etemp = round(sensebrd.get_temperature_from_humidity(), 1)
    sensebrd.show_message("EXT " + str(etemp) + "C", text_colour=[240, 230, 140])
    time.sleep(8)
-   rhraw = sensebrd.get_humidity()
-   rh = round(rhraw, 1)
+   # rhraw = sensebrd.get_humidity()
+   rh = round(sensebrd.get_humidity(), 1)
    sensebrd.show_message("RH % " + str(rh), text_colour=[0, 110, 0])
    time.sleep(30)
